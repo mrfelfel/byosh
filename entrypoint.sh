62402e5be789ffc4366ef1838a7b54e739711621
mrfelfel <qazvinyjavad@gmail.com>
2022-10-18 10:57:25 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 pkill -9 nginx
 /usr/sbin/nginx
 # Start the second process
-/usr/bin/python3 /opt/dns.py --ip ENV --whitelist /opt/domains
+/usr/bin/python3 /opt/dns.py --ip ENV --whitelist all --allow-all
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start dns: $status"
